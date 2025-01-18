0b91cfa9e1e78d7b6a6ece13b68b382536644e67
ViolettaS41 <165622447+ViolettaS41@users.noreply.github.com>
2025-01-18 19:07:49 +0700

    Add files via upload

--- 2023/khranitelpro/khranitelpro/settings.py
+++ 2023/khranitelpro/khranitelpro/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'khranitelpro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'khranitelpro_db',
+        'USER': 'django_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        }
     }
 }
 
